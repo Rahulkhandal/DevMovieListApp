5066eebf785e10712c835b42fb1422e6024d965a
Rahul <khandalrahul03@gmail.com>
2024-06-10 21:22:02 +0530

    Initial Commit

---
+++ DevMovieListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevMovieListApp
+//
+//  Created by Rahul on 10/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
