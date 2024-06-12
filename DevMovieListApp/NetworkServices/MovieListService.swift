c10d86d1ebeb368f1895eef2e4d9ffc07d2701ad
Rahul <khandalrahul03@gmail.com>
2024-06-12 21:17:40 +0530

    Added Spm dependency Created MovieListService

---
+++ DevMovieListApp/NetworkServices/MovieListService.swift
@@ -0,0 +1,22 @@
+//
+//  MovieListService.swift
+//  DevMovieListApp
+//
+//  Created by Rahul on 12/06/24.
+//
+
+import Foundation
+import DevNetworking
+//https://api.themoviedb.org/3/movie/now_playing
+
+protocol FetchLatestMovieListProtocol {
+    
+}
+
+
+final class MovieListService: FetchLatestMovieListProtocol {
+    
+    init(networkModule: NetworkModule) {
+        
+    }
+}
